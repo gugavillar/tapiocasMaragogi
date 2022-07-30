49f61b0b4f179df331134143c4eb4758a7c74edd
Guga Villar <grsv.21@gmail.com>
2022-07-30 15:00:24 -0300

    :hammer: config: add prettier and husky

--- src/constants/data.ts
+++ src/constants/data.ts
@@ -1,50 +1,50 @@
-import md5 from 'md5'
+import { v4 as uuidv4 } from 'uuid'
 
 export const DATA_SALGADAS = [
   {
-    id: md5('Tapioca de coco'),
+    id: uuidv4(),
     title: 'Tapioca de coco',
     src: '/assets/tapioca_de_coco.jpg',
     description: 'Massa de tapioca recheada com coco fresco',
     price: 4.0
   },
   {
-    id: md5('Tapioca de coco e queijo'),
+    id: uuidv4(),
     title: 'Tapioca de coco e queijo',
     src: '/assets/tapioca_de_coco_queijo.jpg',
     description: 'Massa de tapioca recheada com coco fresco e queijo coalho',
     price: 6.0
   },
   {
-    id: md5('Tapioca de calabresa'),
+    id: uuidv4(),
     title: 'Tapioca de calabresa',
     src: '/assets/tapioca_de_calabresa.jpg',
     description: 'Massa de tapioca recheada com coco fresco, queijo coalho e calabresa',
     price: 10.0
   },
   {
-    id: md5('Tapioca de presunto'),
+    id: uuidv4(),
     title: 'Tapioca de presunto',
     src: '/assets/tapioca_de_frango.png',
     description: 'Massa de tapioca recheada com coco fresco, presunto e queijo coalho',
     price: 10.0
   },
   {
-    id: md5('Tapioca de frango'),
+    id: uuidv4(),
     title: 'Tapioca de frango',
     src: '/assets/tapioca_de_frango.png',
     description: 'Massa de tapioca recheada com coco fresco, frango desfiado e queijo coalho',
     price: 10.0
   },
   {
-    id: md5('Tapioca de carne de sol'),
+    id: uuidv4(),
     title: 'Tapioca de carne de sol',
     src: '/assets/carne_de_sol.jpg',
     description: 'Massa de tapioca recheada com coco fresco, carne de sol e queijo coalho',
     price: 10.0
   },
   {
-    id: md5('Tapioca de quatro queijos'),
+    id: uuidv4(),
     title: 'Tapioca de quatro queijos',
     src: '/assets/carne_de_sol.jpg',
     description:
@@ -52,28 +52,28 @@ export const DATA_SALGADAS = [
     price: 11.0
   },
   {
-    id: md5('Tapioca de presunto c/ catupiry'),
+    id: uuidv4(),
     title: 'Tapioca de presunto c/ catupiry',
     src: '/assets/tapioca_de_frango.png',
     description: 'Massa de tapioca recheada com coco fresco, presunto, queijo coalho e catupiry',
     price: 11.0
   },
   {
-    id: md5('Tapioca de frango c/ catupiry'),
+    id: uuidv4(),
     title: 'Tapioca de frango c/ catupiry',
     src: '/assets/tapioca_de_frango.png',
     description: 'Massa de tapioca recheada com coco fresco, frango desfiado, queijo coalho e catupiry',
     price: 11.0
   },
   {
-    id: md5('Tapioca de carne de sol c/ catupiry'),
+    id: uuidv4(),
     title: 'Tapioca de carne de sol c/ catupiry',
     src: '/assets/carne_de_sol.jpg',
     description: 'Massa de tapioca recheada com coco fresco, carne de sol, queijo coalho e catupiry',
     price: 11.0
   },
   {
-    id: md5('Tapioca de calabresa c/ catupiry'),
+    id: uuidv4(),
     title: 'Tapioca de calabresa c/ catupiry',
     src: '/assets/tapioca_de_calabresa.jpg',
     description: 'Massa de tapioca recheada com coco fresco, queijo coalho, calabresa e catupiry',
